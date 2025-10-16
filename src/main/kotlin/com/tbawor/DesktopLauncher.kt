c24cd4f01e00f5d04aa6d7d76df382ca4c69ea9c
Tomasz Bawor <bawortomasz@gmail.com>
2025-10-16 17:52:25 +0200

    Minor changes, and making disabled button work

--- src/main/kotlin/com/tbawor/DesktopLauncher.kt
+++ src/main/kotlin/com/tbawor/DesktopLauncher.kt
@@ -4,8 +4,8 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.tbawor.idle.IdleGame
 
-private const val WINDOW_WIDTH = 500
-private const val WINDOW_HEIGHT = 500
+private const val WINDOW_WIDTH = 800
+private const val WINDOW_HEIGHT = 600
 private const val WINDOW_TITLE = "Idle game"
 
 object DesktopLauncher {
@@ -16,6 +16,7 @@ object DesktopLauncher {
       setTitle(WINDOW_TITLE)
       useVsync(true)
       setWindowedMode(WINDOW_WIDTH, WINDOW_HEIGHT)
+        setResizable(false)
       setForegroundFPS(60)
       setMaximized(true)
     }
